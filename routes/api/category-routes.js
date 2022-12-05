92b5ffada68dc7b8dbee75ea4c491eee3c102dce
mgetz34 <mgetz340@gmail.com>
2022-12-04 20:41:28 -0700

    worked on the get, put, post, and delete routes for the category routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,65 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((CategoryData) => {
+    res.json(CategoryData);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne(
+    {
+      where: {
+        id: req.params.id
+      },
+    }
+  ).then((CategoryData) => {
+    res.json(CategoryData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((newCategory) => {
+      // Send the newly created row as a JSON object
+      res.json(newCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name,
+  },
+    {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
+
 
 module.exports = router;
