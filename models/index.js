299571014cb47d578ead3e4cba660387525771ec
mgetz34 <mgetz340@gmail.com>
2022-12-07 09:36:06 -0700

    un-pluralirzed associations, were coming back undefined

--- models/index.js
+++ models/index.js
@@ -5,21 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Products.belongsTo(Category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
 // Categories have many Products
-Categories.hasMany(Products, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
-Products.belongsToMany(Tags, { through: 'ProductTag' });
+Product.belongsToMany(Tag, { through: 'ProductTag' });
 
 // Tags belongToMany Products (through ProductTag)
-Tags.belongsToMany(Products, { through: 'ProductTag' });
+Tag.belongsToMany(Product, { through: 'ProductTag' });
 
 module.exports = {
   Product,
