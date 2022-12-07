8024687956bef0df98293da5ccfc0eb10b911c33
mgetz34 <mgetz340@gmail.com>
2022-12-07 09:33:03 -0700

    added associations to index.js in the routes folder for categories, products, and tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Categories.hasMany(Products, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, { through: 'ProductTag' });
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, { through: 'ProductTag' });
 
 module.exports = {
   Product,
