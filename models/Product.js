3ec95b56a0b751047bb39d5c0535ba45cde46d19
mgetz34 <mgetz340@gmail.com>
2022-12-07 09:18:28 -0700

    added validation to the price and stock inputs for the product.js model

--- models/Product.js
+++ models/Product.js
@@ -22,13 +22,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
-      isDecimal: true,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      isNumeric: true,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
