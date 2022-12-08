ac9d9c01a10771d77895fde3a3180f11225cb522
mgetz34 <mgetz340@gmail.com>
2022-12-07 20:50:50 -0700

    issues with associactions on the tagRoutes, adjusted, all test good

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,23 +3,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 //http://localhost3001/api/tags
 //GET all tags
-//not working
+//tests good
 router.get('/', async (req, res) => {
-  const tagData = await Tag.findAll();
-  return res.json(tagData);
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //http://localhost3001/api/tags/:id
 //GET tag by id
-//not working
+//tests good
 router.get('/:id', async (req, res) => {
-  const tagData = await Tag.findByPk(req.params.id);
-  return res.json(tagData);
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //http://localhost3001/api/tags
 //CREATE a tag
-//not working
+//tests good
 router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
@@ -31,7 +48,7 @@ router.post('/', async (req, res) => {
 
 //http://localhost3001/api/tags/:id
 //UPDATE a tag
-//not working
+//tests good
 router.put('/:id', async (req, res) => {
   const updatedTag = await Tag.update(
     {
@@ -48,7 +65,7 @@ router.put('/:id', async (req, res) => {
 
 //http://localhost3001/api/tags/:id
 //DELETE a tag
-//not working
+//tests good
 router.delete('/:id', async (req, res) => {
   try {
     const tagData = await Tag.destroy({
